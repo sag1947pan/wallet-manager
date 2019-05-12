51e19082fa0896cc76ae5ec39d806a5d2781d824
Ranjith Junnuthula <48952879+docmanaging@users.noreply.github.com>
2019-05-12 10:52:05 +0530

    UI Changes on Client Account Page

--- src/app/post-reg/post-reg.component.ts
+++ src/app/post-reg/post-reg.component.ts
@@ -139,6 +139,7 @@ export class PostRegComponent implements OnInit {
         this.visible = event;
     }
 
+    //Vertical Tab Content
 
      openCity(evt, cityName) {
     // Declare all variables
@@ -159,7 +160,31 @@ export class PostRegComponent implements OnInit {
     // Show the current tab, and add an "active" class to the link that opened the tab
     document.getElementById(cityName).style.display = "block";
     evt.currentTarget.className += " active";
-}
+    }
+
+    //Horizontal Tab Content
+
+    openHorTab(evt, cityName) {
+        // Declare all variables
+        var i, Horizontaltabcontent, tablinks;
+
+        // Get all elements with class="Horizontaltabcontent" and hide them
+        Horizontaltabcontent = document.getElementsByClassName("Horizontaltabcontent");
+        for (i = 0; i < Horizontaltabcontent.length; i++) {
+            Horizontaltabcontent[i].style.display = "none";
+        }
+
+        // Get all elements with class="tablinks" and remove the class "active"
+        tablinks = document.getElementsByClassName("tablinks");
+        for (i = 0; i < tablinks.length; i++) {
+            tablinks[i].className = tablinks[i].className.replace(" active", "");
+        }
+
+        // Show the current tab, and add an "active" class to the link that opened the tab
+        document.getElementById(cityName).style.display = "block";
+        evt.currentTarget.className += " active";
+    }
+
 
 
 }
