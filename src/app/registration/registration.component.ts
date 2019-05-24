6c2b3a1af15a35a46fa8698e4b59026a27d3eaca
Kartheek Madavapeddi <kartheek.madavapeddi@avid.com>
2019-05-24 20:04:13 +0800

    register buttons

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormsModule, ReactiveFormsModule, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormsModule, ReactiveFormsModule, FormGroup, Validators, FormControl } from '@angular/forms';
 import { WalletService } from 'src/app/wallet.service';
 import { MatFormFieldModule, MatInputModule } from '@angular/material';
 import {MatRadioModule} from '@angular/material/radio';
@@ -16,23 +16,29 @@ export class RegistrationComponent implements OnInit {
     regGroup: FormGroup;
     submitted = false;
     Status: string;
-    public selectedRadio = {"value":"Payee"};
+    public selectedRadio = "Payee";
 
     constructor(
         private formBuilder: FormBuilder,
-       // selectedRadio = {"value":"Payee"},
+        //selectedRadio : new FormControl(), 
         private router: Router,) 
         { }
 
     ngOnInit() {
         this.regGroup = this.formBuilder.group({
-            selectedRadio: ['Payee'],
+            selectedRadio : ['Payee'],//new FormControl(),
         })
     }
     onSubmitRegister(){
-        console.log("form value"+this.regGroup.value.selectedRadio);
-        //console.log("form alon value"+selected);
-        console.log("form alon value"+this.regGroup.get('register'));
+        console.log("form alon value"+this.selectedRadio);
+        console.log("form alon value"+this.regGroup.get('selectedRadio').value);
+        let selValue = this.selectedRadio;
+        if(selValue == 'Payee'){
+            console.log("selValue.."+selValue);
+            this.router.navigate(['/CustReg']);
+        }else if(selValue == 'Bene'){
+            this.router.navigate(['/BeneReg']);
+        }
     }
     onSubmit() {
        // debugger;
