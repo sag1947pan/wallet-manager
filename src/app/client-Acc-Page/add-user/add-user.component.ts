00f26871028d421a2b0a87000d780573d6479dab
Ranjith Junnuthula <48952879+docmanaging@users.noreply.github.com>
2019-05-26 20:53:01 +0530

    User details UI Component UI Changes.

--- src/app/client-Acc-Page/add-user/add-user.component.ts
+++ src/app/client-Acc-Page/add-user/add-user.component.ts
@@ -4,6 +4,27 @@ import { FormBuilder, FormsModule, FormControl, FormGroup, Validators } from '@a
 import { Router } from '@angular/router';
 import { WalletService } from 'src/app/wallet.service';
 import { first } from 'rxjs/operators';
+import { MatSnackBar } from '@angular/material'
+
+// Users details Info
+export interface PeriodicElement {
+    position: number;
+    userId: string;
+    role: string;
+   
+    status: string;
+   
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+    { position: 1, userId: 'veeratest1@test.com', role: 'Admin', status: 'Active' },
+    { position: 2, userId: 'veeratest2@test.com', role: 'Operations', status: 'Active' },
+    { position: 3, userId: 'veeratest3@test.com', role: 'Authorizer', status: 'Active' },
+    { position: 4, userId: 'kartheek2@test.com', role: 'Operations', status: 'InActive' },
+    { position: 5, userId: 'ranjith@test.com', role: 'Authorizer', status: 'InActive' },
+    { position: 6, userId: 'ranjith9@test.com', role: 'Operations', status: 'InActive' },   
+];
+
 
 
 @Component({
@@ -20,11 +41,12 @@ export class AddUserComponent implements OnInit {
     CustomerCode; string;
     SuccessMessage: string;
 
+    displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+    dataSource = ELEMENT_DATA;
+
 
     options: string[] = ['United Kingdom Pounds', 'Singapore Dollars', 'Indian Rupees'];
-
     Industries: string[] = ['Retail', 'Technology', 'Manufacturing', 'Insurance', 'Public Sector', 'Charity / Trust', 'Transportation / Logistics'];
-
     Roles: string[] = ['Admin', 'Operator', 'Authoriser'];
     PaymentTypes: string[] = ['Salary Payments', 'Vendor/ Supplier Payments', 'Rewards/prizes', 'Reimbursements'];
     Countries: string[] = ['India', 'USA', 'UK', 'Singapore', 'Australia'];
@@ -34,9 +56,21 @@ export class AddUserComponent implements OnInit {
 
     constructor(private formBuilder: FormBuilder,
         private router: Router,
-        private WalletService: WalletService,) { }
+        private WalletService: WalletService,
+        private snackBar: MatSnackBar) { }
 
     ngOnInit() {
+
+        //this.WalletService.GetUsersSummary()
+        //    .pipe(first())
+        //    .subscribe(data => {
+        //        console.log("data from data" + data);
+        //        let resBody = {};
+        //        data = JSON.parse(data);
+        //        console.log("data from resBody" + data);
+               
+               
+        //    }, error => (this.error = error));
 
         this.UserInfoGroup = this.formBuilder.group({
 
@@ -70,11 +104,15 @@ export class AddUserComponent implements OnInit {
                 .subscribe(data => {
                     this.Status = data.Status;
                    // this.CustomerCode = data.CustomerCode;
-                    this.SuccessMessage = data.SuccessMessage;
-                   // this.router.navigate(['/ClientReg2']);
-                    alert("User details added successfully" );
+                    this.SuccessMessage = data.SuccessMessage;                  
+                 
+                    this.snackBar.open('User details added successfully' , 'Done', {
+                            duration: 3000,
+                        });
                 }, error => (this.error = error));
         }
     }
+
+    //GET API for Users Summary
 
 }
