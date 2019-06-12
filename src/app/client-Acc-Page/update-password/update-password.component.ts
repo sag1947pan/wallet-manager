633847d220b8777ddd8ce615b2ebd5618c3e744e
Ranjith Junnuthula <48952879+docmanaging@users.noreply.github.com>
2019-06-12 23:58:08 +0530

    Update Password Screen validation

--- src/app/client-Acc-Page/update-password/update-password.component.ts
+++ src/app/client-Acc-Page/update-password/update-password.component.ts
@@ -5,15 +5,15 @@ import { Router } from '@angular/router';
 
 
 @Component({
-  selector: 'app-update-password',
-  templateUrl: './update-password.component.html',
-  styleUrls: ['./update-password.component.css']
+    selector: 'app-update-password',
+    templateUrl: './update-password.component.html',
+    styleUrls: ['./update-password.component.css']
 })
 export class UpdatePasswordComponent implements OnInit {
 
     PasswordGroup: FormGroup;
 
-    hide = true;    
+    hide = true;
 
     constructor(private formBuilder: FormBuilder,
         private router: Router, ) { }
@@ -21,13 +21,23 @@ export class UpdatePasswordComponent implements OnInit {
     ngOnInit() {
 
         this.PasswordGroup = this.formBuilder.group({
-           currPassword: ['', Validators.compose([Validators.required, Validators.minLength(8)])],
-            NewPasword: ['', Validators.compose([Validators.required, Validators.minLength(8)])],
-            confirmNewPwd: ['',Validators.compose([Validators.required, Validators.minLength(8)])],
+            currPassword: ['', Validators.compose([Validators.required, Validators.minLength(8)])],
+            newPasword: ['', Validators.compose([Validators.required, Validators.minLength(8)])],
+            confirmnewPassword: ['', Validators.compose([Validators.required, Validators.minLength(8)])],
             confirmOTP: ['', Validators.required],
 
-        });
+        }, { validator: matchingPasswords('newPasword', 'confirmnewPassword') });
 
-  }
-
+    }
 }
+
+export function matchingPasswords(passwordKey: string, passwordConfirmationKey: string) {
+    return (group: FormGroup) => {
+        let password = group.controls[passwordKey];
+        let passwordConfirmation = group.controls[passwordConfirmationKey];
+        if (password.value !== passwordConfirmation.value) {
+            return passwordConfirmation.setErrors({ mismatchedPasswords: true })
+        }
+    }
+}
+
