32d8ad6e9d3b959f76bc364c15d4e7d703219098
Ranjith Junnuthula <48952879+docmanaging@users.noreply.github.com>
2019-06-17 01:10:54 +0530

    Implemented Logout in Client Account Page Header

--- src/app/client-Acc-Page/home/home.component.ts
+++ src/app/client-Acc-Page/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from 'src/app/services/AuthenticationService';
 import { Router } from '@angular/router';
+import { AdminUserData } from '../AdminData.model';
 
 @Component({
   selector: 'app-home',
@@ -11,11 +12,15 @@ export class HomeComponent implements OnInit {
     authenticationService: AuthenticationService;
 
     name: string;
+    compName: string;
     constructor(private router: Router,) { }
 
     ngOnInit() {
         let userName = sessionStorage.getItem("userName");
         this.name = userName;
+
+        let item = JSON.parse(sessionStorage.getItem("userData")) as AdminUserData;
+        this.compName = item.company_name;
   }
 
     logout() {
