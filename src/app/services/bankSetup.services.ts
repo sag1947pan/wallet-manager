e857549d06be7884ddb110a494e0e3bf0fb68254
Madavapeddi <h100828@in.reach.com>
2019-07-30 21:23:14 +0800

    bank submit

--- src/app/services/bankSetup.services.ts
+++ src/app/services/bankSetup.services.ts
@@ -25,9 +25,14 @@ export class BankSetupServices {
 */
 bankMasterService(RegDetails) {
     const headers = new HttpHeaders().set('content-type', 'application/json');
-    
-    return this.http.post<any>('https://jwetj5otq1.execute-api.eu-west-2.amazonaws.com/dev/customer', RegDetails, {
+    console.log("RegDetails of bank setup is..."+JSON.stringify(RegDetails));
+    return this.http.post<any>('https://f2hj888jj4.execute-api.eu-west-2.amazonaws.com/dev/registerBank', JSON.stringify(RegDetails), {
     // headers
+    params: new HttpParams()
+        .set('Access-Control-Allow-Origin', '*')
+                //.set('userName', this.logUserEmail) userId=5
+        .set('userId', "7"),
+        observe: 'response'
     }).pipe( 
         map((res: any) => {
             console.log("Initial registration..."+JSON.stringify(res));
