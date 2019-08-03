9cb725abd4aaefee00b81baed6281154a0575fd1
Ranjith Junnuthula <48952879+docmanaging@users.noreply.github.com>
2019-08-03 23:40:06 +0530

    Enabled editing in Pricing.

--- src/app/wm-Admin/pricing-info/pricing-info.component.ts
+++ src/app/wm-Admin/pricing-info/pricing-info.component.ts
@@ -22,7 +22,9 @@ export class PricingInfoComponent implements OnInit, ControlValueAccessor {
 
   @ViewChild('agGrid') agGrid: AgGridAngular;
 
-  //pricingInfoGroup:FormGroup
+  //private components;
+  //private rowData;
+  private editType;
 
   private pricingSubscriptionInfo: SubscriptionDetails[] = [
 
@@ -46,7 +48,7 @@ export class PricingInfoComponent implements OnInit, ControlValueAccessor {
 
   private payasYouPkg: PayAsYouGo[] = [
     //private payasYouPkgRows [
-    { noOfTransactions: '0 - 10000', chargesperTransaction: 'GBP 0.5', },
+    { noOfTransactions: '0 - 10000', chargesperTransaction: 'GBP 0.5' },
     { noOfTransactions: '10001 - 50000', chargesperTransaction: 'GBP 0.4', },
     { noOfTransactions: '50001 - 100001', chargesperTransaction: 'GBP 0.35', },
     { noOfTransactions: '200001 - 500000', chargesperTransaction: 'GBP 0.3', },
@@ -57,7 +59,7 @@ export class PricingInfoComponent implements OnInit, ControlValueAccessor {
 
   //Pay As You Go Column definition
   PayasyougoColDefs = [
-    {headerName: 'No Of Transactions', field: 'noOfTransactions', sortable: true, filter: true},
+    {headerName: 'No Of Transactions', field: 'noOfTransactions', sortable: true, filter: true, checkboxSelection: true,},
     {headerName: 'Charges Per Transaction', field: 'chargesperTransaction', sortable: true, filter: true },
     
 ];
@@ -65,17 +67,17 @@ export class PricingInfoComponent implements OnInit, ControlValueAccessor {
 //Subscription Package Columns
 subsPackageColDefs = [
 
-  {headerName: 'Subscription Package', field: 'package', sortable: true, filter: true},
-  {headerName: 'No Of Transactions', field: 'noOfTransactions', sortable: true, filter: true },
-  {headerName: 'Charges Per Month', field: 'chargesperMonth', sortable: true, filter: true },
+  {headerName: 'Subscription Package', field: 'package', sortable: true, filter: true, checkboxSelection: true,},
+  {headerName: 'No Of Transactions', field: 'noOfTransactions', sortable: true, filter: true, editable:true },
+  {headerName: 'Charges Per Month', field: 'chargesperMonth', sortable: true, filter: true, editable:true },
   
 ];
 
 //Fixed Pricing Column definition
 columnDefs = [
-  {headerName: 'Subscription Info', field: 'subscriptionInfo', sortable: true, filter: true,  checkboxSelection: true },
+  {headerName: 'Subscription Info', field: 'subscriptionInfo', sortable: true, filter: true,  checkboxSelection: true, },
   {headerName: 'Currency', field: 'currency', sortable: true, filter: true },
-  {headerName: 'Amount', field: 'amount', sortable: true, filter: true},
+  {headerName: 'Amount', field: 'amount', sortable: true, filter: true, editable:true},
   {headerName: 'Time Period', field: 'validTime', sortable: true, filter: true},
   {headerName: 'Discount', field: 'discount', sortable: true, filter: true}
 ];
@@ -87,18 +89,20 @@ rowData = [
 ];
 
 
+
 public pricingInfoGroup: FormGroup = new FormGroup(
-  {
-  
+  { 
 
     options: new FormControl(""),
    
 });
-  constructor() { }
+  constructor() {
 
-  ngOnInit() {
+   // this.components = { numericCellEditor: this.getNumericCellEditor() };
+  // this.editType = "fullRow";
+   }
 
-  
+  ngOnInit() {  
   }
 
   getSelectedRows() { // Need to check on how to get the selected rows.
@@ -108,6 +112,62 @@ public pricingInfoGroup: FormGroup = new FormGroup(
     alert(`Selected nodes: ${selectedDataStringPresentation}`);
     console.log(selectedDataStringPresentation);
 }
+
+//  getNumericCellEditor() {
+//   function isCharNumeric(charStr) {
+//     return !!/\d/.test(charStr);
+//   }
+//   function isKeyPressedNumeric(event) {
+//     var charCode = getCharCodeFromEvent(event);
+//     var charStr = String.fromCharCode(charCode);
+//     return isCharNumeric(charStr);
+//   }
+//   function getCharCodeFromEvent(event) {
+//     event = event || window.event;
+//     return typeof event.which === "undefined" ? event.keyCode : event.which;
+//   }
+//   function NumericCellEditor() {}
+//   NumericCellEditor.prototype.init = function(params) {
+//     this.focusAfterAttached = params.cellStartedEdit;
+//     this.eInput = document.createElement("input");
+//     this.eInput.style.width = "100%";
+//     this.eInput.style.height = "100%";
+//     this.eInput.value = isCharNumeric(params.charPress) ? params.charPress : params.value;
+//     var that = this;
+//     this.eInput.addEventListener("keypress", function(event) {
+//       if (!isKeyPressedNumeric(event)) {
+//         that.eInput.focus();
+//         if (event.preventDefault) event.preventDefault();
+//       }
+//     });
+//   };
+//   NumericCellEditor.prototype.getGui = function() {
+//     return this.eInput;
+//   };
+//   NumericCellEditor.prototype.afterGuiAttached = function() {
+//     if (this.focusAfterAttached) {
+//       this.eInput.focus();
+//       this.eInput.select();
+//     }
+//   };
+//   NumericCellEditor.prototype.isCancelBeforeStart = function() {
+//     return this.cancelBeforeStart;
+//   };
+//   NumericCellEditor.prototype.isCancelAfterEnd = function() {};
+//   NumericCellEditor.prototype.getValue = function() {
+//     return this.eInput.value;
+//   };
+//   NumericCellEditor.prototype.focusIn = function() {
+//     var eInput = this.getGui();
+//     eInput.focus();
+//     eInput.select();
+//     console.log("NumericCellEditor.focusIn()");
+//   };
+//   NumericCellEditor.prototype.focusOut = function() {
+//     console.log("NumericCellEditor.focusOut()");
+//   };
+//   return NumericCellEditor;
+// }
 
   //ControlValueAccessor Implementation
   public onTouched: () => void = () => {};
