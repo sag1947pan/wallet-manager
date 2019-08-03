1ea396b0d54f4d391a69c357ae9cb4dcf554a6c5
Ranjith Junnuthula <48952879+docmanaging@users.noreply.github.com>
2019-08-03 20:17:06 +0530

    Trading and registered address details

--- src/app/wm-Admin/bank-details/bank-details.component.ts
+++ src/app/wm-Admin/bank-details/bank-details.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit, forwardRef } from '@angular/core';
-import {ControlValueAccessor, NG_VALUE_ACCESSOR, NG_VALIDATORS, Validators, FormGroup, FormBuilder } from '@angular/forms';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR, NG_VALIDATORS, Validators, FormGroup, FormBuilder } from '@angular/forms';
+import { stringify } from '@angular/core/src/render3/util';
 
 @Component({
   selector: 'app-bank-details',
   templateUrl: './bank-details.component.html',
   styleUrls: ['./bank-details.component.css'],
 
-  providers:[
+  providers: [
     {
       provide: NG_VALUE_ACCESSOR,
       useExisting: forwardRef(() => BankDetailsComponent),
@@ -15,12 +16,17 @@ import {ControlValueAccessor, NG_VALUE_ACCESSOR, NG_VALIDATORS, Validators, Form
   ]
 })
 export class BankDetailsComponent implements OnInit, ControlValueAccessor {
-  bankInfoGroup:FormGroup;
- // formBuilder: FormBuilder;
+  bankInfoGroup: FormGroup;
+  // formBuilder: FormBuilder;
 
- Options: string[] = ['Yes', 'No'];
+  IsChecked: boolean;
 
-  constructor(private formBuilder: FormBuilder) { }
+  Options: string[] = ['Yes', 'No'];
+
+  constructor(private formBuilder: FormBuilder) {
+
+    //this.IsChecked = false;
+  }
 
   ngOnInit() {
 
@@ -35,19 +41,19 @@ export class BankDetailsComponent implements OnInit, ControlValueAccessor {
       primaryContactPhoneNo: [''],
 
       //Services and Preferences
-      bankServiceSubscription:[''],
+      bankServiceSubscription: [''],
 
-      servciceActivationStartDate:[''],
-      
-      serviceActivationEndDate:[''],
-      
-      supportReqforBankUserMgmt:[''],
-      
-      supportReqforClientUserMgmt:[''],
-      
-      authManualPymtActivity:[''],
-      
-      authNonPymtActivity:[''],
+      servciceActivationStartDate: [''],
+
+      serviceActivationEndDate: [''],
+
+      supportReqforBankUserMgmt: [''],
+
+      supportReqforClientUserMgmt: [''],
+
+      authManualPymtActivity: [''],
+
+      authNonPymtActivity: [''],
 
 
 
@@ -72,42 +78,81 @@ export class BankDetailsComponent implements OnInit, ControlValueAccessor {
 
       addressLine1: [''],
       addressLine2: [''],
-      cityName: ['', ],
-      state: ['', ],
-      country: ['', ],
-      postCode: ['', ],
+      cityName: ['',],
+      state: ['',],
+      country: ['',],
+      postCode: ['',],
 
       //Registered Address
-      regAddressLine1: ['', ],
+      regAddressLine1: ['',],
       regAddressLine2: [''],
-      regCity: ['', ],
-      regState: ['', ],
-      regCountry: ['', ],
-      regPostCode: ['', ],
+      regCity: ['',],
+      regState: ['',],
+      regCountry: ['',],
+      regPostCode: ['',],
+      //chkboxAddress: ['',],
+    });
 
-    });  
-       
   }
 
-   //ControlValueAccessor Implementation
-   public onTouched: () => void = () => {};
 
-   writeValue(val: any): void {
-     val && this.bankInfoGroup.setValue(val, { emitEvent: false });
-   }
- 
-   registerOnChange(fn: any): void {
-    
-     this.bankInfoGroup.valueChanges.subscribe(fn);
-   }
- 
-   registerOnTouched(fn: any): void {
-     
-     this.onTouched = fn;
-   }
- 
-   setDisabledState?(isDisabled: boolean): void {
-     isDisabled ? this.bankInfoGroup.disable() : this.bankInfoGroup.enable();
-   }
+  showOptions(event) {
+     console.log(event);
+    if (event.checked) {
+
+      this.bankInfoGroup.patchValue(
+        {
+          regAddressLine1: this.bankInfoGroup.value.addressLine1,
+          regAddressLine2: this.bankInfoGroup.value.addressLine2,
+          regCity: this.bankInfoGroup.value.cityName,
+          regState: this.bankInfoGroup.value.state,
+          regpostCode: this.bankInfoGroup.value.postCode,
+          regcountry: this.bankInfoGroup.value.country,
+         
+
+
+        })
+
+
+    }  
+    else
+    {
+      this.bankInfoGroup.reset({
+        regAddressLine1:'',
+        regAddressLine2:'',
+        regCity : '',
+        regState : '',
+        regCountry : '',
+        regpostCode : '',
+
+      })
+      
+    }
+
+
+    console.log(this.bankInfoGroup.value);
+
+  }
+
+  //ControlValueAccessor Implementation
+  public onTouched: () => void = () => { };
+
+  writeValue(val: any): void {
+    val && this.bankInfoGroup.setValue(val, { emitEvent: false });
+  }
+
+  registerOnChange(fn: any): void {
+
+    this.bankInfoGroup.valueChanges.subscribe(fn);
+  }
+
+  registerOnTouched(fn: any): void {
+
+    this.onTouched = fn;
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    isDisabled ? this.bankInfoGroup.disable() : this.bankInfoGroup.enable();
+  }
 
 }
