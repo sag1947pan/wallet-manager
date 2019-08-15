e29e07a56e50e1484b0ae34f7299a1bd79774cf2
Ranjith Junnuthula <48952879+docmanaging@users.noreply.github.com>
2019-08-15 23:29:38 +0530

    Date Validation

--- src/app/wm-Admin/bank-details/bank-details.component.ts
+++ src/app/wm-Admin/bank-details/bank-details.component.ts
@@ -6,12 +6,25 @@ import { Observable } from 'rxjs';
 import { ValidationService } from 'src/app/validations/validation.service';
 import { STATEGROUPS } from 'src/app/countries-List/country-data';
 import { StateGroup } from 'src/app/countries-List/country';
+//For Custom Date Picker
+import {MomentDateAdapter} from '@angular/material-moment-adapter';
+import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from '@angular/material/core';
+import * as moment from 'moment';
 
-//New code for countries List
-// export interface StateGroup {
-//   letter: string;
-//   names: string[];
-// }
+
+
+export const MY_FORMATS = {
+  parse: {
+    dateInput: 'DD/MM/YYYY',
+  },
+  display: {
+    dateInput: 'DD/MM/YYYY',
+    monthYearLabel: 'MMM YYYY',
+    dateA11yLabel: 'DD/MM/YYYY',
+    monthYearA11yLabel: 'MMMM YYYY',
+  },
+};
+
 
 export const _filter = (opt: string[], value: string): string[] => {
   const filterValue = value.toLowerCase();
@@ -31,9 +44,14 @@ export const _filter = (opt: string[], value: string): string[] => {
       provide: NG_VALUE_ACCESSOR,
       useExisting: forwardRef(() => BankDetailsComponent),
       multi: true,
-    }
+    },
+    {provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE]},
+
+    {provide: MAT_DATE_FORMATS, useValue: MY_FORMATS},
   ]
 })
+
+
 export class BankDetailsComponent implements OnInit, ControlValueAccessor {
   bankInfoGroup: FormGroup;
 
@@ -48,7 +66,16 @@ export class BankDetailsComponent implements OnInit, ControlValueAccessor {
   //State Groups.
   stateGroups: StateGroup[] = STATEGROUPS;
   stateGroupOptions: Observable<StateGroup[]>;
-  // stateGroup = new FormControl();
+  
+
+  //Date Min and Max Validation.
+  servciceActivationStartDate = new FormControl('date', [Validators.required]);
+  serviceActivationEndDate =  new FormControl('date', [Validators.required]);
+
+  // minDate is the start of today.
+  minDate = new Date(new Date().setHours(0, 0, 0, 0));
+  //minDate = new Date(2019, 0, 1);
+  maxDate = new Date(2021, 0, 1);
 
   constructor(
     private formBuilder: FormBuilder,
@@ -56,25 +83,22 @@ export class BankDetailsComponent implements OnInit, ControlValueAccessor {
 
   ngOnInit() {
 
-
-
     this.bankInfoGroup = this.formBuilder.group({
 
       //Bank Details
-      // bankId: ['', Validators.compose([Validators.required, Validators.minLength(8)])],     
+         
       bankId: ['', Validators.compose([Validators.required, ValidationService.bicCodeValidator])],
       bankName: ['', Validators.compose([Validators.required, Validators.minLength(0)])],
       primaryContactName: ['', Validators.compose([Validators.required, Validators.minLength(0)])],
-      primaryContactEmailId: ['', Validators.compose([Validators.required, ValidationService.emailValidator])],
-      // primaryContactEmailId: ['', Validators.compose([Validators.required, emailValidator])],
+      primaryContactEmailId: ['', Validators.compose([Validators.required, ValidationService.emailValidator])],      
       primaryContactPhoneNo: ['', Validators.compose([Validators.required, Validators.minLength(11)])],
 
       //Services and Preferences
       bankServiceSubscription: [''],
 
-      servciceActivationStartDate: [''],
+     // servciceActivationStartDate: ['date', Validators.required],
 
-      serviceActivationEndDate: [''],
+     // serviceActivationEndDate: [''],
 
       supportReqforBankUserMgmt: [''],
 
@@ -116,6 +140,39 @@ export class BankDetailsComponent implements OnInit, ControlValueAccessor {
     //     startWith(''),
     //     map(value => this._filterGroup(value))
     //   );
+  }
+
+  //Date Error Messages.
+  getErrorMessage(pickerInput: string): string {
+    if (!pickerInput || pickerInput === '' ) {
+      return 'Please choose a date.';
+    }
+    return this.isMyDateFormat(pickerInput);
+  }
+
+
+  /**
+   * An example of custom validator on a date string.
+   * Valid date is a string, which is:
+   * 1, in the form of YYYY-MM-DD
+   * 2, later than today
+   * 3, not an invalid value like 2018-20-81
+   * @param date a date string
+   */
+  isMyDateFormat(date: string): string {
+    if (date.length !== 10) {
+      return 'Invalid input: Please input a string in the form of DD/MM/YYYY';
+    } else {
+      const da = date.split('-');
+      if (da.length !== 3 || da[0].length !== 4 || da[1].length !== 2 || da[2].length !== 2) {
+        return 'Invalid input: Please input a string in the form of DD/MM/YYYY';
+      } else if (moment(date).isValid()) {
+        return 'Invalid date: Please input a date no later than today';
+      } else if (!moment(date).isValid()) {
+        return 'Invalid date: Please input a date with a valid month and date.';
+      }
+    }
+    return 'Unknown error.';
   }
 
   private _filterGroup(value: string): StateGroup[] {
@@ -208,9 +265,9 @@ export class BankDetailsComponent implements OnInit, ControlValueAccessor {
 
 }
 
-export function emailValidator(control: FormControl): { [key: string]: any } {
-  var emailRegexp = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/;
-  if (control.value && !emailRegexp.test(control.value)) {
-    return { invalidEmail: true };
-  }
-}
+// export function emailValidator(control: FormControl): { [key: string]: any } {
+//   var emailRegexp = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/;
+//   if (control.value && !emailRegexp.test(control.value)) {
+//     return { invalidEmail: true };
+//   }
+// }
