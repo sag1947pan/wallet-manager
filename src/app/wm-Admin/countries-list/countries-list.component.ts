7a47318053817d49ed63d9d7475a6b7e31af3925
Ranjith Junnuthula <48952879+docmanaging@users.noreply.github.com>
2019-08-21 08:54:45 +0530

    countries List for Asia

--- src/app/wm-Admin/countries-list/countries-list.component.ts
+++ src/app/wm-Admin/countries-list/countries-list.component.ts
@@ -22,27 +22,7 @@ import {ContinentCountryGroup} from 'src/app/countries-List/country';
 
 })
 export class CountriesListComponent implements OnInit, ControlValueAccessor{
-    form: FormGroup;
-    // countriesByContinent: any[] = [
-    //     { id: 1, name: 'India',cont:'Asia' },
-    //     { id: 2, name: 'SriLanka',cont:'Asia' },
-    //     { id: 3, name: 'Bangladesh',cont:'Asia' },
-    //     { id: 4, name: 'Pakistan',cont:'Asia' },
-    //     { id: 5, name: 'Nepal', cont: 'Asia' },
-    //     { id: 6, name: 'Marshall Islands', cont: 'Australia' },
-    //     { id: 7, name: 'Micronesia', cont: 'Australia' },
-    //     { id: 8, name: 'Nauru', cont: 'Australia' },
-    //     { id: 9, name: 'New Zealand	', cont: 'Australia' },
-    //     { id: 10, name: 'SouthAfrica', cont: 'Africa' },
-    //     { id: 11, name: 'Nigeria', cont: 'Africa' },
-    //     { id: 12, name: 'Kenya	', cont: 'Africa' },
-    //     { id: 13, name: 'Morocoo', cont: 'Africa' },
-    //     { id: 14, name: 'Mexico', cont: 'NAmerica' },
-    //     { id: 15, name: 'Canada', cont: 'NAmerica' },
-    //     { id: 16, name: 'Unitied States', cont: 'NAmerica' },
-    //     { id: 17, name: 'Cuba', cont: 'NAmerica' },
-    //     { id: 18, name: 'Honduras', cont: 'NAmerica' }
-    // ];
+    form: FormGroup;   
 
     countriesByContinent: any[] = CountryLisyByContinent;
     countryByContFilter: any[];
@@ -50,29 +30,7 @@ export class CountriesListComponent implements OnInit, ControlValueAccessor{
 
 
     stateGroups: StateGroup[] = STATEGROUPS;
-    stateGroupOptions: Observable<StateGroup[]>;
-
-    //countries: any[] = this.stateGroupOptions.forEach()
-
-  //public countriesListGroup: FormGroup = new FormGroup(
- //  {
-    
-
-  //    email: new FormControl("", [Validators.required]),
-  //    firstName: new FormControl("",[Validators.required]),
-  //    middleName: new FormControl("",[Validators.required]),
-  //    lastName: new FormControl("",[Validators.required]),
-  //    role: new FormControl("",[Validators.required])
-  //});
-
-  //countries = new FormControl();
-  //countriesList: string[] = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware',
-  //  'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky',
-  //  'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi',
-  //  'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico',
-  //  'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania',
-  //  'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
-  //  'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
+    stateGroupOptions: Observable<StateGroup[]>; 
 
     
 
